7b76873a22d47ed6fd50bdabc35fb054052b97bc
unknown <Tali@LS-C015.labstyle.local>
2021-10-04 12:55:42 +0300

    reverted computeLevelScores

--- computing_handler.py
+++ computing_handler.py
@@ -7,12 +7,12 @@ def computeLevelScores(users: dict[int, User], levelNum) -> list[UserResult]:
     userResults: list[UserResult] = []
 
     for user in users.values():
-        # if not user.hasSubmittedLevel(levelNum):
-        #     logger.warningLog(f"User {user} hasnt submitted level: {levelNum}")
-        # else:
-        #     submittedLevel = user.getSubmittedLevels(levelNum)
-        #     submittedLevel.score = calculateUserScore(user.username, submittedLevel.levelIndex)
-        userResults.append(UserResult(user.username, 55))
+        if not user.hasSubmittedLevel(levelNum):
+            logger.warningLog(f"User {user} hasnt submitted level: {levelNum}")
+        else:
+            submittedLevel = user.getSubmittedLevels(levelNum)
+            submittedLevel.score = calculateUserScore(user.username, submittedLevel.levelIndex)
+            userResults.append(UserResult(user.username, submittedLevel.score))
 
     return userResults
 
